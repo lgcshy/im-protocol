254f619df1ab06621c643ddf12ffd01f10efe020
nucky <625197382@qq.com>
2022-02-20 14:00:15 +0800

    protocol repo

---
+++ livekit/types.go
@@ -0,0 +1,46 @@
+package livekit
+
+//----------------------------------------------------------------
+
+type TrackID string
+
+func StringsAsTrackIDs(trackIDs []string) []TrackID {
+	asTrackID := make([]TrackID, 0, len(trackIDs))
+	for _, trackID := range trackIDs {
+		asTrackID = append(asTrackID, TrackID(trackID))
+	}
+
+	return asTrackID
+}
+
+//----------------------------------------------------------------
+
+type ParticipantID string
+type ParticipantIdentity string
+type ParticipantName string
+
+type RoomID string
+
+//----------------------------------------------------------------
+
+type RoomName string
+
+func RoomNamesAsStrings(roomNames []RoomName) []string {
+	asString := make([]string, 0, len(roomNames))
+	for _, roomName := range roomNames {
+		asString = append(asString, string(roomName))
+	}
+
+	return asString
+}
+
+func StringsAsRoomNames(roomNames []string) []RoomName {
+	asRoomName := make([]RoomName, 0, len(roomNames))
+	for _, roomName := range roomNames {
+		asRoomName = append(asRoomName, RoomName(roomName))
+	}
+
+	return asRoomName
+}
+
+//----------------------------------------------------------------
---
+++ webhook/webhook_test.go
@@ -0,0 +1,80 @@
+package webhook_test
+
+import (
+	"context"
+	"encoding/json"
+	"net"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/livekit/protocol/auth"
+	"github.com/livekit/protocol/webhook"
+)
+
+func TestWebHook(t *testing.T) {
+	s := newServer(":8765")
+	require.NoError(t, s.Start())
+	defer s.Stop()
+
+	authProvider := auth.NewFileBasedKeyProviderFromMap(map[string]string{
+		"mykey": "mysecret",
+	})
+
+	payload := map[string]interface{}{
+		"test": "payload",
+		"nested": map[string]interface{}{
+			"structure": true,
+		},
+	}
+
+	s.handler = func(r *http.Request) {
+		// receive logic
+		data, err := webhook.Receive(r, authProvider)
+		require.NoError(t, err)
+
+		var decoded map[string]interface{}
+		require.NoError(t, json.Unmarshal(data, &decoded))
+
+		require.EqualValues(t, decoded, payload)
+	}
+
+	notifier := webhook.NewNotifier("mykey", "mysecret", []string{
+		"http://localhost:8765",
+	})
+	require.NoError(t, notifier.Notify(context.Background(), payload))
+}
+
+type testServer struct {
+	handler func(r *http.Request)
+	server  *http.Server
+}
+
+func newServer(addr string) *testServer {
+	s := &testServer{}
+	s.server = &http.Server{
+		Addr:    addr,
+		Handler: s,
+	}
+	return s
+}
+
+func (s *testServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if s.handler != nil {
+		s.handler(r)
+	}
+}
+
+func (s *testServer) Start() error {
+	l, err := net.Listen("tcp", s.server.Addr)
+	if err != nil {
+		return err
+	}
+	go s.server.Serve(l)
+	return nil
+}
+
+func (s *testServer) Stop() {
+	_ = s.server.Shutdown(context.Background())
+}
